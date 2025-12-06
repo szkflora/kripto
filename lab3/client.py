67bcc970656c948929cac54113566335b58a597f
Flora Szallos-Kis <szkisflora@gmail.com>
2025-12-06 19:16:12 +0200

    hmm hmmm

--- lab3/client.py
+++ lab3/client.py
@@ -1,4 +1,35 @@
 from socket import *
+import threading
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import serialization
+
+RUNNING = True
+OTHER_CLIENT_ID = None
+OWN_PUB_KEY = None
+OWN_PRIV_KEY = None
+THEIR_PUBLIC_KEY = None
+
+
+def generate_rsa_keys():
+    private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
+    public_key = private_key.public_key()
+
+    private_pem = private_key.private_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PrivateFormat.PKCS8,
+        encryption_algorithm=serialization.NoEncryption(),
+    )
+
+    public_pem = public_key.public_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PublicFormat.SubjectPublicKeyInfo,
+    )
+
+    return public_pem.decode(), private_pem.decode()
+
+
+def build_message(msg_type, body):
+    return f"{msg_type}\n{body}"
 
 
 def find_free_port(start_port=12001, end_port=65535):
@@ -18,41 +49,130 @@ def connect_socket(name, port):
     return client_socket
 
 
-def main():
-    client_id = find_free_port()
-    server_name = "localhost"
-    server_port = 12000
-    print(
-        """Options:
-            Press 1 to: ask for public key
-            Press 2 to: register to the key server
-            Press 3 to: get public key of another client
-            Press 4 to: send hello to another client
-            Press 5 to: send half secret to another client
-            Press 6 to: send encrypted message to another client
-            Press 7 to: send bye"""
-    )
+def my_listen(client_socket, client_id):
+    global RUNNING
 
-    while True:
+    while RUNNING:
+        conn_socket, addr = client_socket.accept()
+        conn_socket.settimeout(None)
+        message = conn_socket.recv(4096).decode()
+        lines = message.split("\n")
+        message_type = lines[0].strip()
+
+        match message_type:
+            case "hello":
+                print("received hello from another client")
+            case "ack":
+                print("received ack from another client")
+            case "half_secret":
+                print("received half secret")
+            case "message":
+                print("received encrypted message")
+            case "bye":
+                print("received bye from other client")
+                RUNNING = False
+                client_socket.close()
+                return
+
+        conn_socket.close()
+
+
+def my_send(server_name, server_port, client_own_id, client_name):
+    global RUNNING, OTHER_CLIENT_ID, OWN_PUB_KEY, OWN_PRIV_KEY, THEIR_PUBLIC_KEY
+
+    while RUNNING:
         command = input()
+
+        if not RUNNING:
+            break
+
         match command:
             case "1":
-                socket = connect_socket(server_name, server_port)
-                socket.send(str(client_id).encode())
+                OWN_PUB_KEY, OWN_PRIV_KEY = generate_rsa_keys()
+                conn_socket = connect_socket(server_name, server_port)
+                message_body = f"{str(client_own_id)}\n{OWN_PUB_KEY}"
+                message = build_message("register", message_body)
+                conn_socket.send(message.encode())
+                conn_socket.close()
+                print("registered public key with server")
+
             case "2":
-                socket = connect_socket(server_name, server_port)
+                OTHER_CLIENT_ID = input("other client id: ")
+                conn_socket = connect_socket(server_name, server_port)
+                message = build_message("get_key", str(OTHER_CLIENT_ID))
+                conn_socket.send(message.encode())
+
+                response = conn_socket.recv(4096).decode()
+                lines = response.split("\n")
+                message_type = lines[0].strip()
+                message_body = "\n".join(lines[1:]).strip()
+
+                if message_type == "set_key":
+                    print(f"got public key for client {OTHER_CLIENT_ID} from server")
+                    THEIR_PUBLIC_KEY = message_body
+
+                conn_socket.close()
+
             case "3":
-                break
-            case "4":
-                break
+                print("not implemented yet")
+
             case "5":
-                break
+                print("not implemented yet")
+
+            case "4":
+                print("not implemented yet")
+
             case "6":
-                break
-            case "7":
-                break
-        socket.close()
-        break
+                if OTHER_CLIENT_ID:
+                    try:
+                        conn_socket = connect_socket(client_name, int(OTHER_CLIENT_ID))
+                        message = build_message("bye", "")
+                        conn_socket.send(message.encode())
+                        conn_socket.close()
+                        print(f"sent bye to client {OTHER_CLIENT_ID}")
+                    except:
+                        print("peer was not reachable")
+
+                RUNNING = False
+                return
+
+            case _:
+                print("invalid option")
+
+
+def main():
+    client_id = find_free_port()
+    if not client_id:
+        print("could not find free port exiting")
+        return
+
+    print("my id is", client_id)
+
+    server_name = "localhost"
+    client_name = "localhost"
+    server_port = 12000
+    client_port = int(client_id)
+
+    client_socket = socket(AF_INET, SOCK_STREAM)
+    client_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+    client_socket.bind(("", client_port))
+    client_socket.listen(1)
+
+    print(
+        """options:
+            press 1 to register to the key server
+            press 2 to get public key of another client
+            press 3 to send hello to another client
+            press 4 to send half secret to another client
+            press 5 to send encrypted message to another client
+            press 6 to send bye"""
+    )
+
+    listen_thread = threading.Thread(
+        target=my_listen, args=(client_socket, client_id), daemon=True
+    )
+    listen_thread.start()
+    my_send(server_name, server_port, client_id, client_name)
 
 
 if __name__ == "__main__":
--- lab3/server.py
+++ lab3/server.py
@@ -1,26 +1,40 @@
 from socket import *
 
-server_port = 12000
-server_socket = socket(AF_INET, SOCK_STREAM)
-server_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-server_socket.bind(("", server_port))
-server_socket.listen(1)
-print("The server is ready")
 
-key_dictionary = {}
+def build_message(msg_type, body):
+    return f"{msg_type}\n{body}"
 
 
-def create_pub_key():
-    print()
+def listen_and_send(server_socket, key_dictionary):
+    while True:
+        conn_socket, addr = server_socket.accept()
+
+        message = conn_socket.recv(4096).decode()
+        lines = message.split("\n")
+        message_type = lines[0].strip()
+        message_body = "\n".join(lines[1:]).strip()
+        if message_type == "get_key":
+            key = key_dictionary[int(message_body)]
+            response = build_message("set_key", key)
+            conn_socket.send(response.encode())
+            print("server was asked to provide public key for: ", message_body)
+        else:  ## "register"
+            client_id, client_key = message_body.split("\n", 1)
+            key_dictionary[int(client_id)] = client_key
+            print("server received registration from: ", client_id)
+        conn_socket.close()
 
 
-while True:
-    connection_socket, addr = server_socket.accept()
+def main():
+    server_port = 12000
+    server_socket = socket(AF_INET, SOCK_STREAM)
+    server_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+    server_socket.bind(("", server_port))
+    server_socket.listen(1)
+    print("The server is ready")
+    key_dictionary = {}
+    listen_and_send(server_socket, key_dictionary)
 
-    request = connection_socket.recv(1024).decode()
-    if len(request) == 5:
-        print(request)
-    else:
-        print("hmmmmmmmm")
-    connection_socket.close()
-    break
+
+if __name__ == "__main__":
+    main()
